b011233e9448ba301d3411fd568578d944bb2952
Marius <marius@example.com>
2022-07-18 14:47:14 +0300

    articUzd

--- js/main.js
+++ js/main.js
@@ -4,12 +4,34 @@ console.log('main.js');
 // Nusitaikyti
 
 const h1El = document.querySelector('h1');
+const btn1El = document.getElementById('bnt1');
+const btnSpanEl = btn1El.querySelector('span');
+// boolean velevele
+let largeStyleOn = false;
 
 // 1. nusitaikyti i mygtuka
 // 2. uzdeti event listeneri
-// 3. iskviesti konsole log
+btn1El.addEventListener('click', () => {
+  // 3. iskviesti konsole log
+  console.log('paspaudei');
+  toggleStyles();
+});
 
 console.log('h1El ===', h1El);
-// h1El.style.fontSize = '100px';
-// h1El.style.fontWeight = 'normal';
-// h1El.style.marginBottom = '0.3em';
+
+function toggleStyles() {
+  if (largeStyleOn) {
+    h1El.style.marginBottom = '0.3em';
+    h1El.style.fontSize = '35px';
+    h1El.style.fontWeight = 'bold';
+    btnSpanEl.textContent = 'large';
+    largeStyleOn = false;
+    return;
+  }
+
+  h1El.style.marginBottom = '0.3em';
+  h1El.style.fontSize = '100px';
+  h1El.style.fontWeight = 'normal';
+  btnSpanEl.textContent = 'small';
+  largeStyleOn = true;
+}
