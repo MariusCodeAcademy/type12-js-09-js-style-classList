b11ca4b36615a7de05f06c72267365c53ec20209
Marius <marius@example.com>
2022-07-18 13:59:04 +0300

    1 uzd

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 'use strict';
 console.log('main.js');
+
+// Nusitaikyti
+
+const h1El = document.querySelector('h1');
+
+// 1. nusitaikyti i mygtuka
+// 2. uzdeti event listeneri
+// 3. iskviesti konsole log
+
+console.log('h1El ===', h1El);
+// h1El.style.fontSize = '100px';
+// h1El.style.fontWeight = 'normal';
+// h1El.style.marginBottom = '0.3em';
